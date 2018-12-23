d37924983be238e9e149793c45d8d4ee078cd74e
Erik Mueller <e.mueller.mail@gmail.com>
2018-12-23 21:26:13 +0100

    mix format

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -1,24 +1,25 @@
 defmodule Day1 do
-  def part1, do: Enum.sum input()
+  def part1, do: Enum.sum(input())
   def part2, do: calc(input(), [0])
 
-  defp input, do: "day1" |> Utils.read_lines |> Enum.map(&String.to_integer/1)
+  defp input, do: "day1" |> Utils.read_lines() |> Enum.map(&String.to_integer/1)
 
-  defp calc list, acc do
-    potential_dup = Enum.reduce_while(list, acc, fn freq, frequencies ->
-      [last_freq | _] = frequencies
+  defp calc(list, acc) do
+    potential_dup =
+      Enum.reduce_while(list, acc, fn freq, frequencies ->
+        [last_freq | _] = frequencies
 
-      next_freq = last_freq + freq
+        next_freq = last_freq + freq
 
-      if Enum.member?(frequencies, next_freq) do
-        {:halt, next_freq}
-      else
-        {:cont, [next_freq | frequencies]}
-      end
-    end)
+        if Enum.member?(frequencies, next_freq) do
+          {:halt, next_freq}
+        else
+          {:cont, [next_freq | frequencies]}
+        end
+      end)
 
     cond do
-      is_list potential_dup -> calc(list, potential_dup)
+      is_list(potential_dup) -> calc(list, potential_dup)
       true -> potential_dup
     end
   end
